e9872a5cfcdc03d7148e8b381b6b97a44123ae7d
Mahyar Maleki <mahyarmaleki2003@gmail.com>
2023-08-24 16:28:09 +0330

    models package fully implemented all jpa entities defined and connected to the database successfully

---
+++ src/main/java/com/example/ecommercebackend/models/Address.java
@@ -0,0 +1,33 @@
+package com.example.ecommercebackend.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "address")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "address_line_1", nullable = false, length = 512)
+    private String addressLine1;
+
+    @Column(name = "address_line_2", length = 512)
+    private String addressLine2;
+
+    @Column(name = "country", nullable = false, length = 75)
+    private String country;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private LocalUser user;
+
+}
